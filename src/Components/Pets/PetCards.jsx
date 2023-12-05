6af7024179ee8b3b484d936cc2fab3163bcdb335
wyatt thayer <lolcatk@gmail.com>
2023-12-05 12:11:13 -0700

    added pets route and fixed dummy data

--- src/Components/Pets/PetCards.jsx
+++ src/Components/Pets/PetCards.jsx
@@ -3,8 +3,8 @@ import Card from "react-bootstrap/Card";
 
 export default function PetCards() {
 //  const { pets } = useLoaderData();  // Will be added later
-const pets = {
-    petid: '1',
+const pets = [{
+    petId: '1',
     image: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
     name: 'Fido',
     species: 'Dog',
@@ -13,7 +13,7 @@ const pets = {
     state: 'Utah',
     gender: 'male',
     age: '3 months'
-}
+}]
 
   const singlePetCard = pets.map(
     ({ petId, image, name, species, breed, cityname, state, gender, age }) => (
